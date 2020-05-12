f16a23d65b8c01b323b1e863330afdf8d8a5e124
Roger Terrill <lpaddikt@gmail.com>
2020-05-12 15:49:13 -0700

    Basic working status notifier

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+let status = {
+  princess: {
+    meeting: false,
+    camera: false,
+    camerablur: false,
+    voice: false,
+  },
+  gbear: {
+    meeting: false,
+    camera: false,
+    camerablur: false,
+    voice: false,
+  },
+};
+// ROUTES
+app.get('/', (req, res) => {
+  res.render('index.ejs', { status });
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body.person);
+
+  switch (req.body.person) {
+    case 'princess-meeting':
+      status.princess.meeting = !status.princess.meeting;
+      break;
+    case 'princess-camera':
+      status.princess.camera = !status.princess.camera;
+      break;
+    case 'princess-cameraBlur':
+      status.princess.camerablur = !status.princess.camerablur;
+      break;
+    case 'princess-voice':
+      status.princess.voice = !status.princess.voice;
+      break;
+    case 'gbear-meeting':
+      status.gbear.meeting = !status.gbear.meeting;
+      break;
+    case 'gbear-camera':
+      status.gbear.camera = !status.gbear.camera;
+      break;
+    case 'gbear-cameraBlur':
+      status.gbear.camerablur = !status.gbear.camerablur;
+      break;
+    case 'gbear-voice':
+      status.gbear.voice = !status.gbear.voice;
+      break;
+  }
+
+  res.render('index.ejs', { status });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
